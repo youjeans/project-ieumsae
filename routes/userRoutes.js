a19b1a561c724472ef6b653a6f7135f206ab18fd
김희주 <kim-heejoo@gimhuijuui-MacBook-Pro.local>
2024-05-21 22:08:40 +0900

    일기 전송

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/user', userController.addUser);
-router.get('/user/:id', userController.getUser);
-
-module.exports = router;
