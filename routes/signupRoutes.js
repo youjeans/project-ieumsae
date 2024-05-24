3bd6e3ac8236b0ed675b19b872eb9791b9ddc55c
yeoncheong08 <104972673+yeoncheong08@users.noreply.github.com>
2024-05-24 18:02:26 +0900

    회원할퇴 기능 추가, 비밀번호 해싱 추가. 기능 구현 완료

--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const signupController = require('../controllers/signupController');
 
-router.get('/', signupController.displayPage);
+router.get('/', signupController.displaysignupPage);
 router.post('/register', signupController.signup);
+router.get('/deletePage', signupController.displaydeletePage);
+router.post('/delete', signupController.delete);
+router.get('/login', signupController.displayloginPage);
 
 module.exports = router;
