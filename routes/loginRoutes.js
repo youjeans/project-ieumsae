18932624db9df763771f2ae39ccd140f7a3e8c7d
yeoncheong08 <104972673+yeoncheong08@users.noreply.github.com>
2024-05-27 18:15:38 +0900

    로그아웃 버그 수정

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 
 router.get('/', loginController.displayloginPage);
-router.get('/', loginController.logout);
+router.get('/logout', loginController.logout);
 router.get('/signup', loginController.displaysignupPage);
 router.post('/proc', loginController.login);
 
