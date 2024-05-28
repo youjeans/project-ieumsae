abbc9d5bbc9f236b43d593f1e7814d1675ddab24
whitecia <whitecia04@gmail.com>
2024-05-28 16:03:20 +0900

    diaryExchange와 diaryDatabase 일대일 관계 설정

--- models/diaryExchange.js
+++ models/diaryExchange.js
@@ -1,5 +1,6 @@
 const {DataTypes} = require('sequelize');
 const sequelize = require('../config/database');
+const diaryDatabase = require('./diaryDatabase'); // 동기화 설정 위함
 
 const diaryExchange = sequelize.define('diaryExchange',{
     exchangeNum: { // 교환 기록 번호
@@ -7,6 +8,7 @@ const diaryExchange = sequelize.define('diaryExchange',{
         allowNull: false,
         unique: true,
         primaryKey: true
+        // 외래키 설정
     },
     diaryNum: { // 일기 번호
         type: DataTypes.INTEGER,
@@ -15,10 +17,12 @@ const diaryExchange = sequelize.define('diaryExchange',{
     sendNum: { // 송산자 번호
         type: DataTypes.INTEGER,
         allowNull: false,
+        // 외래키 설정
     },
     getNum: { // 수신자 번호
         type: DataTypes.INTEGER,
         allowNull: false,
+        // 외래키 설정
     },
     sendDate: { // 송신 일시
         type: DataTypes.DATE,
@@ -34,5 +38,9 @@ const diaryExchange = sequelize.define('diaryExchange',{
         
     }
 });
+
+// diaryDatebase와 diartExchange 일대일 관계 설정
+diaryDatabase.hasOne(diaryExchange);
+diaryExchange.belongsTo(diaryDatabase);
 
 module.exports = diaryExchange;
