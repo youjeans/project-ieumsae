d3b934885aab6a3907d1abe862d777f3ccc05097
whitecia <whitecia04@gmail.com>
2024-06-06 17:22:29 +0900

    전송자가 자신인 경우에는 보낸 일기를 즉각 확인할 수 있게 변경

--- controllers/diaryPageController.js
+++ controllers/diaryPageController.js
@@ -8,7 +8,7 @@ exports.displayMy = (req, res) => {
     if (!req.session.member || !req.session.member.회원번호) {
         return res.status(401).send("<script> alert('로그인이 필요합니다.'); location.href = '/login';</script>");
     }
-    const query = 'SELECT 일기_송신일, 일기_내용 FROM 일기 WHERE 교환유형 = 0 AND 작성자_번호 = ? '
+    const query = 'SELECT 일기_송신일, 일기_내용 FROM 일기 WHERE 교환유형 = 0 AND 작성자_번호 = ? AND d.일기_송신일 < NOW()';
     const values = [req.session.member.회원번호];
     database.query(query, values, (err, result) => {
         const transResultData = transResult(result);
@@ -29,7 +29,7 @@ exports.displayPage = (req, res) => {
         return res.status(400).send('Invalid exchange');
     }
 
-    const query = 'SELECT u.이름 AS 작성자_이름, d.일기_내용, d.일기_송신일, d.교환유형 FROM 일기 d JOIN 사용자 u ON d.작성자_번호 = u.회원번호 WHERE (d.수신자_번호 = ? AND d.작성자_번호 = ?) OR (d.작성자_번호 = ? AND d.수신자_번호 = ?) AND d.일기_송신일 < NOW() ORDER BY 일기_송신일 DESC';
+    const query = 'SELECT u.이름 AS 작성자_이름, d.일기_내용, d.일기_송신일, d.교환유형 FROM 일기 d JOIN 사용자 u ON d.작성자_번호 = u.회원번호 WHERE ((d.수신자_번호 = ? AND d.작성자_번호 = ?) AND d.일기_송신일 < NOW()) OR ((d.작성자_번호 = ? AND d.수신자_번호 = ?)) ORDER BY 일기_송신일 DESC'; //  
     const values = [req.session.member.회원번호, exchange, req.session.member.회원번호, exchange];
     database.query(query, values, (err, result) => {
         if (err) {
