a631a2999281fd0408528f2137a756705fdd507e
m1nju <sarah115346@gmail.com>
2024-06-11 20:44:49 +0900

    Fix mainpage css, mysql.js

--- config/mysql.js
+++ config/mysql.js
@@ -1,13 +1,27 @@
-var mysql = require("mysql2");
+// 환경 변수 로드를 위한 dotenv 패키지 로드
+require('dotenv').config();
+
+// mysql2 패키지 로드
+var mysql = require('mysql2');
+
+// 환경 변수로부터 MySQL 접속 정보 설정
 var db_info = {
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: "921204",
-    database: "ieumsae",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 };
 
+// MySQL 연결 생성 및 연결
 var db = mysql.createConnection(db_info);
-db.connect();
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ', err);
+        return;
+    }
+    console.log('Connected to MySQL database!');
+});
 
+// db 객체를 모듈로 내보내기
 module.exports = db;
