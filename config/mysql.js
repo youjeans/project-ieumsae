c9221c4d559f147e4e20c3ee52958854a29cd708
youjeans <dbyj2810@naver.com>
2024-06-19 22:35:37 +0900

    원격 db 서버 연결

--- config/mysql.js
+++ config/mysql.js
@@ -1,16 +1,13 @@
-// 환경 변수 로드를 위한 dotenv 패키지 로드
-require('dotenv').config();
-
 // mysql2 패키지 로드
 var mysql = require('mysql2');
 
 // 환경 변수로부터 MySQL 접속 정보 설정
 var db_info = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: '34.22.93.225',
+    port: "3306",
+    user: 'ieumsae',
+    password: 'ieumsaepwd',
+    database: 'ieumsae'
 };
 
 // MySQL 연결 생성 및 연결
@@ -26,4 +23,3 @@ db.connect((err) => {
 // db 객체를 모듈로 내보내기
 
 module.exports = db;
-
